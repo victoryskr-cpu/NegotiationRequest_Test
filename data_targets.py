aa9540c10e372c63134631c4f0bda4166ae022e1
victoryskr-cpu <victoryskr@gmail.com>
2026-03-12 21:00:22 +0900

    Update target sites list with new URLs

--- data_targets.py
+++ data_targets.py
@@ -1,8 +1,49 @@
-
-# 테스트용 대상 사이트 목록
+# 테스트 대상 지자체 목록
+# 형식: (지자체명, 검색URL)
 
 TARGET_SITES = [
-    ("서울_성북", "https://www.sb.go.kr/www/selectEminwonList.do?searchKrwd=교섭"),
+    ("서울_강북구", "https://www.gangbuk.go.kr/portal/bbs/B0000245/list.do?menuNo=200082&bbsId=&cl1Cd=&optn5=&pageIndex=1&searchCnd2=&searchCnd=&searchWrd=%EA%B5%90%EC%84%AD"),
+    ("서울_관악구", "https://www.gwanak.go.kr/site/gwanak/ex/bbsNew/List.do?typeCode=1&searchCondition=TITLE&searchKeyword=%EA%B5%90%EC%84%AD"),
+    ("서울_서대문구", "https://www.sdm.go.kr/news/notice/notice.do?mode=list&srchTitle=%EA%B5%90%EC%84%AD"),
+    ("서울_성북", "https://www.sb.go.kr/www/selectEminwonList.do?key=6977&searchCnd=all&depNm=&searchCnd2=notAncmtSj&pageUnit=10&bgnde=&endde=&searchKrwd=%EA%B5%90%EC%84%AD"),
+    ("서울_송파구", "https://www.songpa.go.kr/www/selectGosiList.do?key=2776&not_ancmt_se_code=&searchCnd=SJ&searchKrwd=교섭"),
+    ("서울_양천구", "https://www.yangcheon.go.kr/site/yangcheon/ex/bbs/List.do?cbIdx=254#"),
+    ("서울_종로구", "https://www.jongno.go.kr/portal/bbs/selectBoardList.do?bbsId=BBSMSTR_000000000271&menuNo=1756&menuId=1756"),
+    ("서울_중구", "https://www.junggu.seoul.kr/content/boards/62/list?search_column=title&search_keyword=교섭"),
+
+    ("부산_사상구", "https://www.sasang.go.kr/tour/board/list.sasang?boardId=BBS_0000001&menuIdx=75&searchCnd=0&searchWrd=%EA%B5%90%EC%84%AD"),
+
+    ("대구광역시", "https://www.daegu.go.kr/index.do?menu_id=00000052&srchVal=%EA%B5%90%EC%84%AD&srchKey=sj"),
+    ("대구_남구", "https://nam.daegu.kr/index.do?menu_id=00000851"),
+    ("대구_중구", "https://www.jung.daegu.kr/new/pages/administration/page.html?mc=0159"),
+
+    ("울산_남구", "https://www.ulsannamgu.go.kr/cop/bbs/selectSaeolGosiList.do"),
+    ("울산_동구", "https://www.donggu.ulsan.kr/donggu/contents/contents.do?mId=4040100"),
+    ("울산_북구", "https://www.bukgu.ulsan.kr/lay1/S1T86C456/contents.do"),
+    ("울산_울주군", "https://www.ulju.ulsan.kr/ulju/saeol/gosi/list.do?mId=0403010000"),
+    ("울산_중구", "https://www.junggu.ulsan.kr/index.ulsan?menuCd=DOM_000000102004001000"),
+
+    ("경기_고양", "https://www.goyang.go.kr/www/link/BD_notice.do?se=01"),
+    ("경기_광주", "https://www.gjcity.go.kr/portal/saeol/gosi/list.do?mId=0202010000"),
     ("경기_남양주", "https://www.nyj.go.kr/www/selectEminwonWebList.do?key=2492"),
-    ("경북_포항", "https://www.pohang.go.kr/portal/saeol/gosi/list.do"),
+    ("경기_평택", "https://www.pyeongtaek.go.kr/pyeongtaek/saeol/gosi/list.do?mid=0401020100"),
+
+    ("경북_경주", "https://www.gyeongju.go.kr/open_content/ko/page.do?mnu_uid=423"),
+    ("경북_경산", "https://www.gbgs.go.kr/open_content/ko/page.do?mnu_uid=2160&"),
+    ("경북_김천", "https://www.gc.go.kr/portal/saeol/gosi/list.do?mId=1202180100"),
+    ("경북_안동", "https://www.andong.go.kr/portal/saeol/gosi/list.do?mId=0401010000"),
+    ("경북_구미", "https://www.gumi.go.kr/portal/saeol/gosi/list.do?seCode=01&mid=0401040000"),
+    ("경북_포항", "https://www.pohang.go.kr/portal/saeol/gosi/list.do?mid=0202010000"),
+
+    ("충남_공주", "https://www.gongju.go.kr/prog/saeolGosi/GOSI_01/sub04_03_01/list.do"),
+    ("충남_당진", "https://www.dangjin.go.kr/kor/sub03_02_01_01.do"),
+    ("충남_보령", "https://www.brcn.go.kr/prog/eminwon/kor/BB/sub04_03_01/list.do"),
+    ("충남_천안", "https://www.cheonan.go.kr/kor/sub02_02_01.do"),
+
+    ("충북_괴산", "https://www.goesan.go.kr/www/selectBbsNttList.do?key=604"),
+    ("충북_단양", "https://www.danyang.go.kr/dy21/976"),
+    ("충북_영동", "https://www.yd21.go.kr/kr/html/sub02/020103.html?mode=L"),
+    ("충북_증평", "http://www.jp.go.kr/kor/sub03_01_03.do"),
+    ("충북_청주", "https://www.cheongju.go.kr/www/selectEminwonNoticeList.do?key=281&searchKrwd=%EA%B5%90%EC%84%AD"),
+    ("충북_충주", "https://www.chungju.go.kr/www/selectEminwonList.do?key=510&ancmt_sj=%EA%B5%90%EC%84%AD"),
 ]
